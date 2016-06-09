9bf792eb60f7f1d769e7559b32e9165bf8845382
coffee-or-tea <13718918104@163.com>
2016-06-09 09:58:47 +0800

    简单的界面已有，hex与ascl

---
+++ 串口助手/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace 串口助手
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.comboBoxPorts = new System.Windows.Forms.ComboBox();
+            this.buttonOpenPort = new System.Windows.Forms.Button();
+            this.buttonClosePort = new System.Windows.Forms.Button();
+            this.buttonSendData = new System.Windows.Forms.Button();
+            this.textBoxReceivedData = new System.Windows.Forms.TextBox();
+            this.textBoxSendData = new System.Windows.Forms.TextBox();
+            this.comboBoxBaudrate = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // comboBoxPorts
+            // 
+            this.comboBoxPorts.FormattingEnabled = true;
+            this.comboBoxPorts.Location = new System.Drawing.Point(562, 12);
+            this.comboBoxPorts.Name = "comboBoxPorts";
+            this.comboBoxPorts.Size = new System.Drawing.Size(121, 20);
+            this.comboBoxPorts.TabIndex = 0;
+            // 
+            // buttonOpenPort
+            // 
+            this.buttonOpenPort.Location = new System.Drawing.Point(562, 102);
+            this.buttonOpenPort.Name = "buttonOpenPort";
+            this.buttonOpenPort.Size = new System.Drawing.Size(121, 23);
+            this.buttonOpenPort.TabIndex = 1;
+            this.buttonOpenPort.Text = "打开串口";
+            this.buttonOpenPort.UseVisualStyleBackColor = true;
+            this.buttonOpenPort.Click += new System.EventHandler(this.buttonOpenPort_Click);
+            // 
+            // buttonClosePort
+            // 
+            this.buttonClosePort.Location = new System.Drawing.Point(562, 154);
+            this.buttonClosePort.Name = "buttonClosePort";
+            this.buttonClosePort.Size = new System.Drawing.Size(121, 23);
+            this.buttonClosePort.TabIndex = 2;
+            this.buttonClosePort.Text = "关闭串口";
+            this.buttonClosePort.UseVisualStyleBackColor = true;
+            this.buttonClosePort.Click += new System.EventHandler(this.buttonClosePort_Click);
+            // 
+            // buttonSendData
+            // 
+            this.buttonSendData.Location = new System.Drawing.Point(562, 286);
+            this.buttonSendData.Name = "buttonSendData";
+            this.buttonSendData.Size = new System.Drawing.Size(121, 23);
+            this.buttonSendData.TabIndex = 3;
+            this.buttonSendData.Text = "发送";
+            this.buttonSendData.UseVisualStyleBackColor = true;
+            this.buttonSendData.Click += new System.EventHandler(this.buttonSendData_Click);
+            // 
+            // textBoxReceivedData
+            // 
+            this.textBoxReceivedData.Location = new System.Drawing.Point(12, 12);
+            this.textBoxReceivedData.Multiline = true;
+            this.textBoxReceivedData.Name = "textBoxReceivedData";
+            this.textBoxReceivedData.Size = new System.Drawing.Size(544, 219);
+            this.textBoxReceivedData.TabIndex = 4;
+            // 
+            // textBoxSendData
+            // 
+            this.textBoxSendData.Location = new System.Drawing.Point(13, 287);
+            this.textBoxSendData.Name = "textBoxSendData";
+            this.textBoxSendData.Size = new System.Drawing.Size(543, 21);
+            this.textBoxSendData.TabIndex = 5;
+            // 
+            // comboBoxBaudrate
+            // 
+            this.comboBoxBaudrate.FormattingEnabled = true;
+            this.comboBoxBaudrate.Location = new System.Drawing.Point(562, 55);
+            this.comboBoxBaudrate.Name = "comboBoxBaudrate";
+            this.comboBoxBaudrate.Size = new System.Drawing.Size(121, 20);
+            this.comboBoxBaudrate.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(695, 394);
+            this.Controls.Add(this.comboBoxBaudrate);
+            this.Controls.Add(this.textBoxSendData);
+            this.Controls.Add(this.textBoxReceivedData);
+            this.Controls.Add(this.buttonSendData);
+            this.Controls.Add(this.buttonClosePort);
+            this.Controls.Add(this.buttonOpenPort);
+            this.Controls.Add(this.comboBoxPorts);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ComboBox comboBoxPorts;
+        private System.Windows.Forms.Button buttonOpenPort;
+        private System.Windows.Forms.Button buttonClosePort;
+        private System.Windows.Forms.Button buttonSendData;
+        private System.Windows.Forms.TextBox textBoxReceivedData;
+        private System.Windows.Forms.TextBox textBoxSendData;
+        private System.Windows.Forms.ComboBox comboBoxBaudrate;
+    }
+}
+
---
+++ 串口助手/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 串口助手
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+            Array.Sort(ports);
+            comboBoxPorts.Items.AddRange(ports);
+            comboBoxPorts.SelectedIndex = comboBoxPorts.Items.Count > 0 ? 0 : -1;
+            comboBoxBaudrate.SelectedIndex = comboBoxBaudrate.Items.IndexOf("38400");//选择波特率  
+        }
+
+        private void buttonOpenPort_Click(object sender, EventArgs e)
+        {
+            serialPort1.PortName = comboBoxPorts.Text;
+            serialPort1.BaudRate = int.Parse(comboBoxBaudrate.Text);
+            try
+            {
+                serialPort1.Open();
+                buttonClosePort.Enabled = true;
+                buttonOpenPort.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                //捕获到异常信息
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void buttonClosePort_Click(object sender, EventArgs e)
+        {
+            serialPort1.Close();
+            buttonOpenPort.Enabled = true;
+            buttonClosePort.Enabled = false;   
+        }
+
+        private void buttonSendData_Click(object sender, EventArgs e)
+        {
+            //定义一个变量，记录发送了几个字节  
+            int n = 0;
+            //16进制发送  
+            //if (rdb_tx_hex.Checked)
+            //{
+            //    string s = richTextBox_tx.Text.Replace(" ", "");
+            //    byte[] buffer = new byte[s.Length / 2];
+            //    for (int i = 0; i < s.Length; i += 2)
+            //        buffer[i / 2] = (byte)Convert.ToByte(s.Substring(i, 2), 16);
+            //    //转换列表为数组后发送  
+            //    serialPort1.Write(buffer.ToArray(), 0, buffer.Length);
+            //    //记录发送的字节数  
+            //    n = buffer.Length;
+            //}
+            //else//ascii编码直接发送  
+            //{
+            //    serialPort1.Write(richTextBox_tx.Text);
+            //    n = richTextBox_tx.Text.Length;
+            //}
+        }
+    }
+}
